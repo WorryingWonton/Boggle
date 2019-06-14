d5215897d44d41a6525756a60a5809f9d0ffa266
Alan Campbell <ahcampbellster@gmail.com>
2019-06-14 07:29:42 -0700

    Change BoggleInterface.display_board() to adjust spacing based on the length of the longest top_letter on the Board

--- boggle_cl_interface.py
+++ boggle_cl_interface.py
@@ -4,14 +4,19 @@ class BoggleInterface:
         self.game_instance = game_instance
 
     def display_board(self):
+        longest_face_len = self.find_longest_string()
         for row in self.game_instance.board.spaces:
             r_string = ''
             for space in row:
-                if space.cube.top_letter == 'Qu':
-                    r_string += space.cube.top_letter + ' '
-                else:
-                    r_string += space.cube.top_letter + '  '
+                r_string += space.cube.top_letter + ' ' * (longest_face_len - len(space.cube.top_letter) + 1)
             print(r_string)
+
+    def find_longest_string(self):
+        cur_longest = 0
+        for row in self.game_instance.board.spaces:
+            for space in row:
+                cur_longest = max(len(space.cube.top_letter), cur_longest)
+        return cur_longest
 
     def display_scores(self):
         [print(f'{x.name}: {x.score}') for x in self.game_instance.players]
