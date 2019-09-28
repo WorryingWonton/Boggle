8e1603edcf62598a1222a6ef85646bf99bd3c2c8
Alan Campbell <ahcampbellster@gmail.com>
2019-09-28 05:29:46 -0700

    Refactored boggle_tests.py
    Removed all previous test cases, as they worked in a way which bypassed much of my core code
    
    Added new helper methods to help automate some of the testing process and ensure that my code is more fully testsed

--- boggle_tests.py
+++ boggle_tests.py
@@ -1,79 +1,120 @@
 import unittest
-from itertools import permutations
-from boggle import Boggle, Player
-from math import sqrt
+from boggle import Boggle
 
-#Unit Test specific version of Boggle which overrides check_if_valid_english(), but is otherwise unaltered.
-class UTBoggle(Boggle):
+"""
+I want to fully validate my Boggle code.
+I should test:
+    -There is a way to verify is a word is correct per the rules of Boggle for a variety of board sizes and aspect ratios
+    -There is a mechanism for verifying that a word is valid english, and that said word is soored according to a pre-supplied model
+    -That the same word chosen by two or more players will not count towards their score
+    -That the game will run for a pre-set number of rounds
+    -That the game supports one or more players
+    -That the game will return the winning Player's name and score after the last round has expired.
+"""
 
-    def find_word(self, word, trace_word):
-        for row in self.board.spaces:
-            for space in row:
-                if self.trace_path(trace_word, space, set()):
-                    return True
-        return False
-
-
-class TestBoggle(unittest.TestCase):
-
-    def test_find_word_2x2(self):
-        game_instance = UTBoggle(grid_size=(2, 2), max_rounds=1)
-        test_board = self.generate_test_board('NSSN')
-        game_instance = self.map_tls_to_board(game_instance, test_board)
-        my_string = 'NSSN'
-        words = []
-        self.assertEqual(False, game_instance.find_word(None, 'SSS'))
-        self.assertEqual(False, game_instance.find_word(None, 'SSSNN'))
-        self.assertEqual(False, game_instance.find_word(None, 'NNN'))
-        for r in range(1, 5):
-            words += [''.join(x) for x in list(permutations(my_string, r=r))]
-        for word in words:
-            self.assertEqual(True, game_instance.find_word(trace_word=word, word=None))
-
-    def test_find_word_3x3(self):
+class TestIfValidBoggle(unittest.TestCase):
+    """
+    Tests:
+        -A single letter can be found on a board containing a single letter
+        -A single letter can be found on a board containing multiple letters
+        -A word can be found consisting of letters in a horizonal line.
+        -A word can be found consisting of letters in a vertical line.
+        -A word can be found consisting of diagonal letters
+        -(2+c)x(2+r) behavior (any board with non-edge/corner spaces)
+            -2x2
+            -3x3
+            -4x4
+            -6x6
+            -3x4
+            -4x3
+            -1x5
+            -5x1
+            -10x2
+            -2x10
+        -A word can be found using a combination of horizontal, vertical, and diagonal moves
+        -A word will not be counted if it uses the same letter on the board more than once
+        -A word will not be counted if part of it is not on the board
+    """
+    def test_single_letter_board(self):
         pass
 
-    def test_find_word_4x4(self):
-        game_instance = UTBoggle(grid_size=[4, 4], max_rounds=1)
-        test_board = self.generate_test_board('MPOWXYZTFABCNLSH')
-        game_instance = self.map_tls_to_board(game_instance, test_board)
-        word = 'MPOW'
-        self.assertEqual(True, game_instance.find_word(None, word))
-        tb_2 = self.generate_test_board('BZ@IXRIFJCEMPTRA')
-        gi_2 = UTBoggle(grid_size=[4, 4], max_rounds=1)
-        gi_2 = self.map_tls_to_board(gi_2, tb_2)
-        self.assertEqual(True, gi_2.find_word(None, '@IZ'))
-        self.assertEqual(True, gi_2.find_word(None, 'ART'))
-        self.assertEqual(True, gi_2.find_word(None, 'PTJC'))
-        self.assertEqual(True, gi_2.find_word(None, 'BZ@II'))
+    def test_find_single_letter_amongst_many(self):
+        pass
 
-    def test_boggle_score_round(self):
-        game_instance = UTBoggle(max_rounds=3, grid_size=[5, 5])
-        game_instance.players = [Player(name='ll'), Player(name='kk')]
-        game_instance.players[0].words = {0: {'hello': 2, 'hiii': 1}, 1: {'hey': 1, 'there': 2}, 2: {'soo': 1, 'what': 1}}
-        game_instance.players[1].words = {0: {'hello': 2, 'xxxxxx': 3}, 1: {'hey': 1, 'tharr': 2}, 2: {'soo': 1, 'nutttt': 3}}
-        game_instance.score_round()
-        self.assertEqual(1, game_instance.players[0].score)
-        self.assertEqual(3, game_instance.players[1].score)
+    def test_find_horizontal_letters(self):
+        """Uses a 1 x 10 board
+            -Checks left to right
+            -Checks right to left
+            -Verifies a word must be fully on the board
+        """
 
-    def map_tls_to_board(self, game_instance, test_board_tls):
-        for r_idx, row in enumerate(game_instance.board.spaces):
-            for s_idx, space in enumerate(row):
-                space.cube.top_letter_lc = test_board_tls[r_idx][s_idx].lower()
-                space.cube.top_letter = test_board_tls[r_idx][s_idx]
-        return game_instance
+    def test_find_vertical_letters(self):
+        """Uses a 10x1 board
+            -Checks bottom to top
+            -Checks top to bottom
+            -Verifies a word must be fully on the board"""
 
-    def generate_test_board(self, string):
-        #string must always be a perfect square in length
-        root_length = int(sqrt(len(string)))
-        if sqrt(len(string)) % 1 != 0:
-            raise Exception(f'len(string) must be a perfect square, {len(string)} is not a perfect square.')
-        else:
-            string_array = []
-            for i in range(root_length):
-                string_array.append(list(string[0:root_length]))
-                string = string[root_length:]
-            return string_array
+
+class HelperMethods:
+    @staticmethod
+    def configure_board_for_test(test_string, boggle_instance):
+        string_array = HelperMethods.generate_string_array(test_string, boggle_instance.x_width, boggle_instance.y_width)
+        return HelperMethods.map_string_array_to_board(string_array, boggle_instance)
+
+    @staticmethod
+    def generate_string_array(test_string, columns, rows):
+        """
+        :param test_string: A string that is rows*columns in length
+        :param columns: The number of columns on the Board
+        :param rows: The number of rows on the Board
+        :return: An array of character arrays
+        """
+        string_array = []
+        index = 0
+        for i in range(rows):
+            string_array.append([])
+            for j in range(columns):
+                string_array[i].append(test_string[index])
+                index += 1
+        return string_array
+
+    @staticmethod
+    def map_string_array_to_board(string_array, boggle_instance):
+        """
+        :param letter_array: An array of character arrays (generated by HelperMethods.generate_string_array)
+        :param boggle_instance: A Boggle object
+        :return: A Boggle object whose Board's Space's Cube's top_letter parameters have been made to match
+        corresponding values in letter_array
+        """
+        for r_idx, row in enumerate(boggle_instance.board.spaces):
+            for c_idx, space in enumerate(row):
+                space.cube.top_letter = string_array[r_idx][c_idx]
+        return boggle_instance
+
+
+class TestHelperMethods(unittest.TestCase):
+    """
+    This class inspects the Helper Methods for Boggle's unit tests.
+    """
+
+    def test_generate_string_array(self):
+        test_string = 'abcdefghijklmnop'
+        target = [['a', 'b', 'c', 'd'], ['e', 'f', 'g', 'h'], ['i', 'j', 'k', 'l'], ['m', 'n', 'o', 'p']]
+        self.assertEqual(HelperMethods.generate_string_array(test_string, 4, 4), target)
+
+    def test_map_string_array_to_board(self):
+        test_string = 'abcdefghijklmnop'
+        string_array = HelperMethods.generate_string_array(test_string, 4, 4)
+        boggle_instance = Boggle((4, 4), 1)
+        boggle_instance_tl_mod = HelperMethods.map_string_array_to_board(string_array, boggle_instance)
+        for r_idx, row in enumerate(string_array):
+            for c_idx, column in enumerate(row):
+                """
+                The below test checks that each letter, in each row of string_array, matches the corresponding Space's
+                Cube's top_letter parameter on the Board.
+                """
+                self.assertEqual(boggle_instance_tl_mod.board.spaces[r_idx][c_idx].cube.top_letter, string_array[r_idx][c_idx])
+
 
 if __name__ == '__main__':
     unittest.main()
